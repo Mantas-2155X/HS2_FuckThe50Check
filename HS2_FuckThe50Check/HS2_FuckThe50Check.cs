58465891546181fccba8df733ea3301a9153f1d3
Mantas-2155X <syenergyy@gmail.com>
2020-08-07 11:47:40 +0300

    initial

---
+++ HS2_FuckThe50Check/HS2_FuckThe50Check.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace HS2_FuckThe50Check
+{
+    [BepInProcess("HoneySelect2")]
+    [BepInPlugin(nameof(HS2_FuckThe50Check), nameof(HS2_FuckThe50Check), VERSION)]
+    public class HS2_FuckThe50Check : BaseUnityPlugin
+    {
+        public const string VERSION = "1.0.0";
+
+        private void Awake() => Harmony.CreateAndPatchAll(typeof(HS2_FuckThe50Check), nameof(HS2_FuckThe50Check));
+
+        [HarmonyPrefix, HarmonyPatch(typeof(Manager.GameSystem), "IsPathAdd50")]
+        public static bool GameSystem_IsPathAdd50_Bypass(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
